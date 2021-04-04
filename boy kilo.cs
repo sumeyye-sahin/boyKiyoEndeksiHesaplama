90c68c7a464976933af4c306d65e6c9028532b4e
Sümeyye Şahin <58478124+sumeyye-sahin@users.noreply.github.com>
2021-04-04 16:04:11 +0300

    Add files via upload

---
+++ boy kilo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ilkProjem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Boyunuzu metre cinsinden giriniz: ");
+            double boy = Convert.ToDouble(Console.ReadLine());
+            Console.Write("kilonuzu kilogram cinsinden giriniz: ");
+            double kilo = Convert.ToDouble(Console.ReadLine());
+            double bki =  kilo / (boy*boy);
+            Console.Write(bki);
+
+       
+            Console.ReadKey();
+        }
+    }
+}
